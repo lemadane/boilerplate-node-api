31df047181d23e36ee19947298a0eca4b02650ea
lemadane <lem@email.com>
2018-08-12 13:28:50 +0800

    add db config

--- server.js
+++ server.js
@@ -3,16 +3,50 @@ const bodyParser = require('body-parser')
 const routes = require('./routes')
 const log = require('fancy-log')
 const { dev } = require('./env.json')
+const httpLog = require('morgan-body')
+const cors = require('cors')
+const db = require('./lib/db')
 
-const api = express()
-api.use(bodyParser.urlencoded({ extended: true }))
-api.use(bodyParser.json())
-api.use('/api/v1', routes)
+try {
+      const api = express()
+      httpLog(api)
 
-api.listen(dev.webPort, dev.webHost, err => {
-      if (err) {
-            log.error('Error starting server. ' + err.message)
-      } else {
-            log(`Server is listening on, http://${dev.webHost}:${dev.webPort}`)
-      }
-})
+      api.use(bodyParser.urlencoded({ extended: true }))
+      api.use(bodyParser.json())
+      api.use('/api/v1', routes)
+      api.use(cors())
+
+      const env = dev
+
+      db.configure({
+            client: env.db.client,
+            version: env.db.version,
+            connection: {
+                  host: env.db.host,
+                  user: env.db.user,
+                  password: env.db.password,
+                  database: env.db.name
+            },
+            pool: {
+                  min: env.db.minClients,
+                  max: env.db.maxClients
+            },
+            acquireConnectionTimeout: env.db.idleTimeoutMillis
+            
+      }).catch(err => {
+            throw { message: 'Database config error: ' + err.message }
+      })
+
+      api.listen(
+            env.http.port,
+            env.http.host, err => {
+                  if (err) {
+                        throw { message: 'Error starting server. ' + err.message }
+                  } else {
+                        log(`Server is listening on, http://${env.http.host}:${env.http.port}`)
+                  }
+            })
+
+} catch (err) {
+      log.error(err.message)
+}
